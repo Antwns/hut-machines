df342e4c90a9169235e4da04b0d7496cfe84e7f1
Antwns V <Antwns@hotmail.com>
2025-09-18 16:40:20 +0300

    Added test admin command to create a machine at the target location

--- src/main/kotlin/hut/dev/hutmachines/HutMachinesPlugin.kt
+++ src/main/kotlin/hut/dev/hutmachines/HutMachinesPlugin.kt
@@ -1,3 +1,4 @@
+// src/main/kotlin/hut/dev/hutmachines/HutMachinesPlugin.kt
 package hut.dev.hutmachines
 
 import hut.dev.hutmachines.commands.buildHmRoot
@@ -8,12 +9,19 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class HutMachinesPlugin : JavaPlugin() {
     override fun onEnable() {
+        // Load configs + register specs
         ConfigWorker.loadAll(this)
         MachineSpecRegistry.registerAll(this)
 
+        // Register Brigadier command with Paper lifecycle
         lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { event ->
-            event.registrar().register(buildHmRoot(this))   // <-- pass 'this'
+            val node = buildHmRoot(this)   // <-- pass plugin
+            logger.info("[HutMachines] About to register /${node.literal} with children: ${node.children.map { it.name }}")
+            // simplest: node + a description string
+            event.registrar().register(node, "HutMachines root command")
+
         }
-        logger.info("Registered Paper Brigadier command: /hm delete")
+
+        logger.info("Registered Paper Brigadier command: /hm (mark, delete)")
     }
 }
